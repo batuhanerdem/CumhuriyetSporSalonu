e9eec4e231dcd67388dcbe8a569aab1ca35225a1
Batuhan <batuhanerdem173@gmail.com>
2024-07-22 05:34:16 +0300

    hayat gercekten zor

--- app/src/main/java/com/example/cumhuriyetsporsalonu/domain/use_case/RegisterUseCase.kt
+++ app/src/main/java/com/example/cumhuriyetsporsalonu/domain/use_case/RegisterUseCase.kt
@@ -1,11 +1,13 @@
 package com.example.cumhuriyetsporsalonu.domain.use_case
 
+import android.util.Log
 import com.example.cumhuriyetsporsalonu.data.remote.repository.FirebaseRepository
 import com.example.cumhuriyetsporsalonu.data.remote.repository.Uid
 import com.example.cumhuriyetsporsalonu.domain.model.User
 import com.example.cumhuriyetsporsalonu.utils.Resource
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
@@ -17,23 +19,18 @@ class RegisterUseCase @Inject constructor(private val repository: FirebaseReposi
         name: String,
         surname: String,
     ): Flow<Resource<Uid>> = flow {
-        repository.register(email, password).collect { result ->
-            when (result) {
-                is Resource.Success -> {
-                    result.data?.let {
-                        val myUser = User(it, email, name, surname)
-                        repository.setUser(myUser).collect { result ->
-                            when (result) {
-                                is Resource.Success -> emit(Resource.Success(it))
-                                else -> emit(Resource.Error(message = result.message))
-                            }
-                        }
-                    }
-                }
-
-                else -> emit(result)
-            }
+        repository.register(email, password).flatMapConcat { result ->
+            if (result !is Resource.Success) return@flatMapConcat flow { emit(result) }
+            result.data ?: return@flatMapConcat flow { }
+            emit(Resource.Success(result.data))
+            val myUser = User(result.data, email, name, surname)
+            repository.setUser(myUser)
+        }.collect {
+            Log.d("tag", "execute: $it ${it.data} ${it.message}")
+            if (it is Resource.Error) emit(Resource.Error(it.message))
         }
     }
 
 }
+
+
